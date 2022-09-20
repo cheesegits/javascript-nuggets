e4d264c3d764c7423aab9886626ba38f496e91ac
Matt Mulit <cheesegits@gmail.com>
2022-09-20 16:27:55 -0400

    map tutorial completed

--- 01-map/app.js
+++ 01-map/app.js
@@ -26,20 +26,25 @@ const people = [
 // does not change the size of original array (unlike filter)
 // uses values from original array when making new one
 
-const getAges = (person) => person.age * 2;
-
-const ages = people.map(getAges);
+// Ex. 1: single line
+const ages = people.map((person)=> person.age);
 console.log(ages);
 
-const newPeople = people.map((item) => {
-  return {
-    firsName: item.name.toUpperCase(),
-    oldAge: item.age + 20,
-  };
-});
-console.log(newPeople);
+// Ex. 2: using as a reference
+const getAges = (person) => person.age;
+const ages2 = people.map(getAges);
+console.log(ages2)
 
-const names = people.map((person) => `<h2>${person.name}</h2>`);
+// Ex. 3: returning an object
+const newPeople = people.map((person) => {
+return {
+  firstName: person.name.toUpperCase(),
+  oldAge: person.age + 20
+}
+})
+console.log(newPeople)
+ 
+// Ex. 4: posting to HTML
+const names = people.map((person)=> `<h2>${person.name}</h2>`);
 const result = document.querySelector('#result');
-
 result.innerHTML = names.join('');
