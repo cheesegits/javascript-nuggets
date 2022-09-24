2e0fcb4b6e4969f94bd119972ee434fc1d792bac
Matt Mulit <cheesegits@gmail.com>
2022-09-24 10:50:49 -0400

    object destructuring tutorial completed

--- 05-object-destructuring/app.js
+++ 05-object-destructuring/app.js
@@ -8,23 +8,31 @@ const bob = {
     sister: 'jane',
   },
 };
-const {
-  first: firstName,
-  last,
-  city,
-  zip,
-  siblings: { sister: favoriteSibling },
-} = bob;
-console.log(firstName, last, city, zip, favoriteSibling);
 
-// const firstName = bob.first;
-// const lastName = bob.last;
-// const sister = bob.siblings.sister;
+// creating variables through dot notation
+const firstName = bob.first;
+const lastName = bob.last;
+const sister = bob.siblings.sister; // nested object
 
-// console.log(firstName,lastName,sister);
+console.log(firstName, lastName, sister);
 
+// curly braces for destructuring object
+// unlike destructuring arrays, order doesn't matter because directly accessing properties by name
+const {last: shakeAndBake, first, city, zip, siblings:{sister:favoriteSibling}} = bob; // renaming variable with :alias
+console.log(first, city, zip, shakeAndBake, favoriteSibling); // zip is undefined because property name must be in original object
+
+// destructuring inside function body
 function printPerson(person) {
-  console.log(person.first);
+  const {first, last} = person
+  console.log(first, last);
 }
 
 printPerson(bob);
+
+// destructuring object inside parameter
+// used often with React components/props
+function printPerson2({first, last, city, siblings:{sister}}) {
+  console.log(first, last, city, sister);
+}
+
+printPerson2(bob);
